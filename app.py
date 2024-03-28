196500e061c7d895edb323b2bc90947b6aa73f1d
MichaelWK04 <michaelwalker1358@gmail.com>
2024-03-28 00:26:34 -0400

    Created css for template, cleaned up all CSS files

--- app.py
+++ app.py
@@ -3,10 +3,41 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
 def home():
     pagename='home'
     return render_template('home.html', pagename=pagename)
+
+@app.route('/assignments')
+def assignments():
+    return render_template('assignments.html')
+
+@app.route('/calendar')
+def calendar():
+    return render_template('calendar.html')
+
+@app.route('/courseteam')
+def courseteam():
+    return render_template('courseteam.html')
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
+
+@app.route('/lab')
+def lab():
+    return render_template('lab.html')
+
+@app.route('/lectures')
+def lectures():
+    return render_template('lectures.html')
+
+@app.route('/resources')
+def resources():
+    return render_template('resources.html')
+
+@app.route('/tests')
+def tests():
+    return render_template('tests.html')
 
 if __name__ == '__main__':
     app.run()
